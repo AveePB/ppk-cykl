8b218717363f7fa17a4f71f8a4df1f85feef7473
AveePB <mike303.py@gmail.com>
2024-11-16 09:47:53 +0100

    Added program query loop and menu

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@
 #include "inparams.h"
 #include "graphs.h"
 
+// Declare constants
+const char SAVE_AND_EXIT = '0';
+const char ADD_NEW_VERTICE = '1';
+const char REMOVE_VERTICE = '2';
+const char ADD_NEW_EDGE = '3';
+const char REMOVE_EDGE = '4';
+const char UPDATE_EDGE_WEIGHT = '5';
+const char DETECT_CYCLES = '6';
+const char FIND_SHORTEST_PATH = '7';
+const char FIND_LONGEST_PATH = '8';
+
+/**
+ * @brief Prints out a help menu to the console.
+ */
+void printHelpMenu();
+
 int main(int argc, char** argv) {
 	// Load file arguments
 	std::vector<std::string> args(argv, argv + argc);
@@ -17,19 +33,70 @@ int main(int argc, char** argv) {
 	if (!isDataLoaded) {
 		std::string ans;
 		std::cout << "Niewczytano pliku z grafem...\n";
-		std::cout << "Jesli chcesz kontynuowac, napisz \"tak\" ";
+		std::cout << "Jesli chcesz kontynuowac, napisz \"tak\": ";
 		std::cin >> ans;
 
 		// Exit if not accepted
-		if (ans != "tak") return 0;
+		if (ans == "tak") isDataLoaded = true;
+	}
+
+	// Graph query loop
+	while (isDataLoaded) {
+		// Declare variables
+		char command;
+
+		// Reset terminal window
+		system("cls");
+		printHelpMenu();
+		std::cin.clear();
+
+		// Function luncher
+		std::cout << "Wykonaj funkcje: ";
+		std::cin >> command;
+		if (std::cin.fail()) continue;
+
+		// Task handler
+		if (command == SAVE_AND_EXIT) {
+			break;
+		}
+
+		else if (command == ADD_NEW_VERTICE) { }
+
+		else if (command == REMOVE_VERTICE) { }
+		
+		else if (command == ADD_NEW_EDGE) { }
+
+		else if (command == REMOVE_EDGE) { }
+		
+		else if (command == UPDATE_EDGE_WEIGHT) { }
+
+		else if (command == FIND_SHORTEST_PATH) { }
+
+		else if (command == FIND_LONGEST_PATH) { }
+
 	}
 
 	// Save current graph state
 	graph.save(inparams::outFile);
 
-	char c;
-	std::cin >> c;
-	
-
 	return 0;
+}
+
+void printHelpMenu() {
+	std::cout << "================== POMOC ==================\n";
+	std::cout << "Program do zarzadzania grafem skierowanym.\n";
+	std::cout << "-------------------------------------------\n";
+	std::cout << "Funkcje programu:\n";
+	std::cout << "0. Zapisz i zakoncz\n";
+	std::cout << "1. Dodaj nowy wierzcholek\n";
+	std::cout << "2. Usun wierzcholek\n";
+	std::cout << "3. Dodaj nowa krawedz\n";
+	std::cout << "4. Usun krawedz\n";
+	std::cout << "5. Edytuj wage krawedzi\n";
+	std::cout << "6. Wyznacz cykle w grafie\n";
+	std::cout << "7. Wyznacz najkrotsza sciezke cykliczna\n";
+	std::cout << "8. Wyznacz najdluzsza sciezke cykliczna\n";
+	std::cout << "-------------------------------------------\n";
+	std::cout << "Aby uruchomic dana funkcje, podaj jej numer.\n";
+	std::cout << "===========================================\n";
 }
